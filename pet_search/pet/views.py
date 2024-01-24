f68308f08d200525da4d8e7e02d3627e5199dbea
Ivan Zavyalov <126201116+VannyZav@users.noreply.github.com>
2024-01-25 00:57:51 +0300

    добваил код в html файлы. Откорректировал views.py

--- pet_search/pet/views.py
+++ pet_search/pet/views.py
@@ -6,11 +6,13 @@ from .models import Animal
 
 def all_animal(request):
     animals = Animal.objects.all()
-    return render(request, 'all_animals.html', {'animals':animals })
+    return render(request, 'all_animals.html', {'animals': animals})
+
 
 def animal_detail(request):
-    animal = Animal.objects.get(pk = animal_id)
+    animal = Animal.objects.get(pk='animal_id')
     return render(request, 'animal_detail.html', {'animal': animal})
+
 
 def add_animal(request):
     if request.method == 'POST':
